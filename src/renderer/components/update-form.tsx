4a766873c1b738a6f2019a569aa2770234da3b29
Daniel Sávio <spnjc137@gmail.com>
2024-01-19 23:41:20 -0300

    front-end: Creating the front end logic to upload a file

--- src/renderer/components/update-form.tsx
+++ src/renderer/components/update-form.tsx
@@ -1,13 +1,15 @@
 import React, { ReactElement, useState, useRef, ChangeEventHandler } from 'react';
 const ipRegex1 = '(\d+\.\d+.\d+.\d+):\d{5}$'
 const ipRegex2 = '(\d+\.\d+.\d+.\d+)$'
-
+const file = new File([''],"")
 ​
 export function UpdateForm() {
 
 
   const [sdp, setSdp] = useState<string[]>(['192.168.3.121']);
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [systemFile, setSystemFile] = useState<File>(file);
+
   const inputIpRef = useRef("null");
   const ipErrorRef = useRef("null");
   
@@ -66,6 +68,15 @@ export function UpdateForm() {
     window.electron.send('system-update',sdp)
   }
 
+  function handleFile(e: any){
+    if(e.target.files){
+      setSystemFile(e.target.files[0])
+    }else{
+      setSystemFile(file)
+    }
+
+  }
+
   return (
     <div
       id="update-form"
@@ -74,33 +85,30 @@ export function UpdateForm() {
       <h1 className="text-xl font-bold mb-1"> Natan</h1>
       <h1 className="text-lg font-bold mb-5"> Atualização de sistema do SD+</h1>
 
+      <div className='mb-5'>
+        <label className="block mb-2 text-md rounded-md cursor-pointer pl-2 pr-2 pt-1 pb-1 bg-sky-600 text-slate-50" htmlFor="system-file">Upload arquivo de sistema (.sdu)</label>
+        <span className='text-slate-600 dark:text-slate-50'> {`Arquivo: ${systemFile.name}`} </span>
+        <input onChange={handleFile} accept=".sdu" type="file" id="system-file" className="hidden"/>
+      </div>
+
       <div className="mb-10 input-group justify-center align-bottom flex">
-        <label
-          htmlFor="ip-sdp"
-          className="rounded-l-lg p-1 font-bold text-slate-50 bg-slate-500"
-        >
-          {' '}
+        <label htmlFor="ip-sdp" className="rounded-l-lg p-1 font-bold text-slate-50 bg-slate-500">
           IP
-          {' '}
         </label>
-        <input
-          ref={inputIpRef}
-          onChange={handleIp}
+
+        <input ref={inputIpRef} onChange={handleIp}
           type="text"
           id="ip-sdp"
           placeholder='ex.: 192.168.3.121'
           className="text-slate-800 h-fit text-center p-1  bg-slate-200"
         />
-        <button
-          id="add-sdp-update"
-          onClick={addArray}
-          className=" p-1 rounded-r-lg font-bold text-slate-50  bg-treetech-700 b-round w-8"
-        >
+        <button id="add-sdp-update" onClick={addArray} className=" p-1 rounded-r-lg font-bold text-slate-50  bg-treetech-700 b-round w-8">
           +
         </button>
       </div>
 
       <span className='text-red-500 block mb-1'>{errorMessage}</span>
+
 
       <button onClick={removeAll} className='text-slate-50 p-1 m-2 bg-red-700 rounded-md' > Limpar lista </button>
 
